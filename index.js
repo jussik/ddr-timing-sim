f0a9b6b8424032f1a4022b6270db9c4fad161a00
Jussi Kosunen <deathcarrot@gmail.com>
2020-11-15 14:14:16 +0200

    tweaked ui +summary + flush data at end

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
     const casCommands = ["RD", "RDA", "WR", "WRA"];
 
     window.update = function update() {
-        const freqMhz = +clockFreq.value;
-        // map timing names to times, e.g. {CL:14,tRCD:16,...}
-        const timings = [].reduce.call(
-            document.getElementsByClassName("timing"),
-            (o, e) => (o[e.name] = +e.value, o),
-            {});
+        const freqMhz = +i_CK.value;
+        const timings = {}; // map timing names to times, e.g. {CL:14,tRCD:16,...}
+
+        function cyclesToTime(n) {
+            return parseFloat((n * 1000 / freqMhz).toFixed(1)) + "ns";
+        }
+
+        o_CK.textContent = freqMhz * 2 + " MT";
+        for (let e of document.getElementsByClassName("timing")) {
+            let n = +e.value;
+            timings[e.name] = n;
+            window["o_" + e.name].textContent = cyclesToTime(n);
+        }
 
         let nextData, nextDataCk;
         const deferredData = []; // e.g. data burst CL cycles after RD
@@ -25,19 +32,20 @@
             DQ: { wave: [], data: [] }
         };
 
-        let counter = 0;
+        let ckCounter = 0;
+        let dataCounter = 0;
 
         function emitCommand(cmd, args = {}) {
             if (casCommands.includes(cmd)) {
                 deferredData.push(
-                    { offset: 0, ck: counter + timings.CL },
-                    { offset: 2, ck: counter + timings.CL + 1 },
-                    { offset: 4, ck: counter + timings.CL + 2 },
-                    { offset: 6, ck: counter + timings.CL + 3 });
+                    { offset: 0, ck: ckCounter + timings.CL },
+                    { offset: 2, ck: ckCounter + timings.CL + 1 },
+                    { offset: 4, ck: ckCounter + timings.CL + 2 },
+                    { offset: 6, ck: ckCounter + timings.CL + 3 });
             }
 
-            ticks.push(counter);
-            times.push((counter * 1000 / freqMhz).toFixed(1) + "ns");
+            ticks.push(ckCounter);
+            times.push(cyclesToTime(ckCounter));
             waves.CK.wave.push(".1.0");
             waves.Command.data.push(cmd);
             if (cmd === "DES" && args.warp) {
@@ -45,32 +53,34 @@
                 times.push("\u21DD");
                 waves.Command.wave.push("=.x|");
                 waves.DQ.wave.push(".x.|");
-                if (nextDataCk === counter) {
-                    console.error(`Warp during data burst! (time ${counter})`);
+                if (nextDataCk === ckCounter) {
+                    console.error(`Warp during data burst! (time ${ckCounter})`);
                 }
             } else {
                 ticks.push("\u200B");
                 times.push("\u200B");
                 waves.Command.wave.push(cmd === "DES" ? "=.x." : "5.x.");
-                if (nextDataCk === counter) {
+                if (nextDataCk === ckCounter) {
                     waves.DQ.wave.push(".=.=");
                     waves.DQ.data.push("D" + nextData.offset, "D" + (nextData.offset + 1));
                     shiftData();
+                    dataCounter += 2;
                 } else {
                     waves.DQ.wave.push(".x..");
                 }
             }
 
-            if (nextData && nextDataCk <= counter) {
-                console.error(`Data burst overlap! (time ${nextDataCk} to ${counter + 1})`);
-                while (nextDataCk <= counter) {
+            if (nextData && nextDataCk <= ckCounter) {
+                console.error(`Data burst overlap! (time ${nextDataCk} to ${ckCounter + 1})`);
+                while (nextDataCk <= ckCounter) {
                     shiftData();
                 }
             }
+
             if (args.counter)
-                counter = args.counter;
+                ckCounter = args.counter;
             else
-                counter++;
+                ckCounter++;
         }
 
         function wait(time) {
@@ -85,12 +95,12 @@
             time = Math.floor(time);
 
             if (time > 1) {
-                const finalDes = counter + time - 2; // generate (time - 1) DES commands
-                while (counter < finalDes) {
-                    let waitLeft = finalDes - counter;
+                const finalDes = ckCounter + time - 2; // generate (time - 1) DES commands
+                while (ckCounter < finalDes) {
+                    let waitLeft = finalDes - ckCounter;
                     if (waitLeft < 2) {
                         // short wait, skip warp
-                        while (counter < finalDes) {
+                        while (ckCounter < finalDes) {
                             emitCommand("DES");
                         }
                     } else if (!nextData) {
@@ -98,7 +108,7 @@
                         emitCommand("DES", { warp: true, counter: finalDes });
                     } else {
                         // upcoming data
-                        let minWarp = counter + 2;
+                        let minWarp = ckCounter + 2;
                         let nextTarget = Math.min(nextDataCk, finalDes);
                         if (nextTarget >= minWarp) {
                             // warp as far as possible
@@ -131,6 +141,18 @@
             }
         });
 
+        // flush remaining data bursts
+        if (!nextData) {
+            shiftData();
+        }
+        const lastData = deferredData.length ? deferredData[deferredData.length - 1] : nextData;
+        if (lastData) {
+            const timeToWait = lastData.ck - ckCounter + 3;
+            if (timeToWait > 1) {
+                wait(timeToWait);
+            }
+        }
+
         const waveObj = {
             signal: [
                 { name: 'CK', wave: 'l.' + waves.CK.wave.join(""), period: 0.5, phase: 0.675 },
@@ -142,6 +164,15 @@
         };
         signaljson.textContent = JSON.stringify(waveObj, null, 2);
         WaveDrom.RenderWaveForm(0, waveObj, "wave", false);
+
+        const suffix = ["", "K", "M", "G", "T"];
+        function formatSi(n) {
+            const logDivisor = Math.min(Math.floor(Math.log10(n) / 3), suffix.length - 1);
+            return parseFloat((n / Math.pow(10, 3 * logDivisor)).toFixed(1)) + suffix[logDivisor];
+        }
+
+        const bitsPerSecond = dataCounter * freqMhz * 1000000 / ckCounter;
+        summary.textContent = `${formatSi(dataCounter)}b in ${cyclesToTime(ckCounter)}, ${formatSi(bitsPerSecond)}b/s`;
     }
 
     update();
